220a1eb46280d5ca9af61ce425964969c7309545
evans <karlinevans333@gmail.com>
2019-11-07 16:27:18 +0700

    template2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-// const userRoutes = require('./userRoutes')
+const userRoutes = require('./userRoutes')
 // const todoRoutes = require('./todoRoutes')
 
-// router.use('/',userRoutes)
+router.use('/',userRoutes)
 // router.use('/todo',todoRoutes)
 
 module.exports = router
