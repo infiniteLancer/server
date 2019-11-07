7ec8a43e00e27b1823b0b1dbec2f7d2ac3f2d25f
evans <karlinevans333@gmail.com>
2019-11-07 18:46:13 +0700

    auth done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,50 +1,50 @@
 const {verifyToken} = require('../helpers/jwt')
-// const User = require('../models/user')
-// const Todo = require('../models/todo')
+const User = require('../models/user')
+const Vancancy = require('../models/vacancy')
 
 function authentication(req, res, next){
-    // try {
-    //     let decodedToken = verifyToken(req.headers.token)
-    //     User.findById(decodedToken._id)
-    //         .then(user => {
-    //             if(user){
-    //                 req.loggedUser = decodedToken
-    //                 next()
-    //             }
-    //             else{
-    //                 next({
-    //                     status: 401,
-    //                     message: 'Authentication Failed'
-    //                 })
-    //             }
-    //         })
-    //         .catch(next)
-    // }
-    // catch(err) {
-    //     next({ 
-    //         status: 401,
-    //         message: err 
-    //     })
-    // }
+    try {
+        let decodedToken = verifyToken(req.headers.token)
+        User.findById(decodedToken._id)
+            .then(user => {
+                if(user){
+                    req.loggedUser = decodedToken
+                    next()
+                }
+                else{
+                    next({
+                        status: 401,
+                        message: 'Authentication Failed'
+                    })
+                }
+            })
+            .catch(next)
+    }
+    catch(err) {
+        next({ 
+            status: 401,
+            message: err 
+        })
+    }
 }
 
-function authorizationTodo(req, res, next){
-    // Todo.findById(req.params.id)
-    //     .then(todo => {
-    //         if(!todo){
-    //             next({ status: 404, message: 'Not Found' })
-    //         }
-    //         else if(todo.userId == req.loggedUser._id){
-    //             next()
-    //         }
-    //         else{
-    //             next({ 
-    //                 status: 403, 
-    //                 message: 'Not Authorized' 
-    //             })
-    //         }
-    //     })
-    //     .catch(next)
+function authorizationVancancy(req, res, next){
+    Vancancy.findById(req.params.id)
+        .then(vancancy => {
+            if(!vancancy){
+                next({ status: 404, message: 'Not Found' })
+            }
+            else if(vancancy.UserId == req.loggedUser._id){
+                next()
+            }
+            else{
+                next({ 
+                    status: 403, 
+                    message: 'Not Authorized' 
+                })
+            }
+        })
+        .catch(next)
 }
 
-module.exports = {authentication, authorizationTodo} 
+module.exports = {authentication, authorizationVancancy} 
